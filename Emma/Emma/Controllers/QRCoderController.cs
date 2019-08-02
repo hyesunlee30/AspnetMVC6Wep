9466abc37ef380840b9a682ea5cf6a3f200f9df5
emmalee1990 <emma@pineapple.im>
2019-08-02 20:25:18 +0900

    QRCoder 기능 구현중

---
+++ Emma/Emma/Controllers/QRCoderController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetNote.MVC6.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using System.IO;
+    using QRCoder;
+    using System.Drawing;
+
+   
+    public class QRCoderController : Controller
+    {
+        public ActionResult Index()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("www.naver.com", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            var bitmapBytes = BitmapToBytes(qrCodeImage); //Convert bitmap into a byte array
+            return File(bitmapBytes, "image/jpeg"); //Return as file result
+        }
+        // This method is for converting bitmap into a byte array
+        private static byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
+    
+}
